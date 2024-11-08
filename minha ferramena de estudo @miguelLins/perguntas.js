184e283a3d20cb6f3d167a164ce38cf760639b2f
Miguel Lins <00001121690506sp@al.educacao.sp.gov.br>
2024-11-08 11:30:24 -0300

    Add files via upload

---
+++ minha ferramena de estudo @miguelLins/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Digital Card?',
+    'O Digital Card é uma linguagem de programação'
+)
+
+criaCartao(
+    'Historia',
+    'Qual foi os anos de começo da primeira e da segunda guerra mundial?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz como voce esta? em Inglês?',
+    'Como voce esta  em ingles é How are you? (RAI)'
+)
